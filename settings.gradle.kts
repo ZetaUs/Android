90797864ee4769cd6942eb58e9f17389de640742
ZZTX <3809026566@qq.com>
2026-06-29 15:44:34 +0800

    Update

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,7 +12,7 @@ pluginManagement {
     }
 }
 plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.1"
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
@@ -22,6 +22,6 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "众志通讯"
+rootProject.name = "Shop"
 include(":app")
  
