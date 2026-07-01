3fb2539b3019ad3ccffb6509f08cf2230a4e8a73
ZZTX <3809026566@qq.com>
2026-07-01 12:11:15 +0800

    Update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,11 +2,9 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
 }
-
 android {
     namespace = "com.zztx.shop"
     compileSdk = 36
-
     defaultConfig {
         applicationId = "com.zztx.shop"
         minSdk = 24
@@ -16,7 +14,6 @@ android {
         versionName = "1.0.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
-
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -26,22 +23,14 @@ android {
             )
         }
     }
-
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-
-    // 放在同一个android{}内部，不要单独写 android{...}
-    kotlinOptions {
-        jvmTarget = "11"
-    }
-
     viewBinding {
         enable = true
     }
 }
-
 dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
@@ -50,7 +39,6 @@ dependencies {
     implementation("androidx.activity:activity-ktx:1.9.0")
     implementation("io.coil-kt:coil:2.6.0")
     implementation("com.google.code.gson:2.10.1")
-
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
