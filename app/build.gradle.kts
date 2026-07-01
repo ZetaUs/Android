f0306b0cd38376ae43043c7c99c6af26f331f87b
ZZTX <3809026566@qq.com>
2026-07-01 12:06:46 +0800

    Update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,9 +32,9 @@ android {
         targetCompatibility = JavaVersion.VERSION_11
     }
 
-    // 替换原来报错的 kotlinOptions，改用标准kotlin块
-    kotlin {
-        jvmToolchain(11)
+    // 放在同一个android{}内部，不要单独写 android{...}
+    kotlinOptions {
+        jvmTarget = "11"
     }
 
     viewBinding {
@@ -43,20 +43,15 @@ android {
 }
 
 dependencies {
-    //noinspection UseTomlInstead
     implementation("androidx.core:core-ktx:1.13.1")
-    //noinspection UseTomlInstead
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.activity:activity-ktx:1.9.0")
     implementation("io.coil-kt:coil:2.6.0")
-    // Gson依赖，解决Gson找不到
-    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("com.google.code.gson:2.10.1")
 
-    //noinspection UseTomlInstead
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
-    //noinspection UseTomlInstead
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 }
